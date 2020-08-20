c017763cb8d5352bf8961c86d2d33bf3ea532068
mia1232 <774640444@qq.com>
2020-08-21 09:25:38 +1000

    pic modify

--- src/runtime-canvas/index.js
+++ src/runtime-canvas/index.js
@@ -40,14 +40,7 @@ const renderer = createRenderer({
   insert(el, parent) {
     parent.addChild(el);
   },
-  // 新加
-  // 处理注释
-  createComment() {},
-  // 获取父节点
-  parentNode() {},
-  // 获取兄弟节点
-  nextSibling() {},
-  // 删除节点时调用
+
   remove(el) {
     const parent = el.parent;
     if (parent) {
